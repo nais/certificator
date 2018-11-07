446ab82e3646948b38f48e809c74b66529d6c594
Kim Tore Jensen <kimtjen@gmail.com>
2018-11-07 10:58:57 +0100

    Allow separating downloads into production and development

--- install-certs.sh
+++ install-certs.sh
@@ -6,7 +6,7 @@ function dcurl {
     popd > /dev/null
 }
 
-function download_certs {
+function download_certs_prod {
     dcurl "$1" "http://crl.adeo.no/crl/eksterne/webproxy.nav.no.crt"
 
     dcurl "$1" "http://crl.adeo.no/crl/A01PKIROOT2012_NAV%20Root%20CA(1).crt"
@@ -14,7 +14,9 @@ function download_certs {
     dcurl "$1" "http://crl.adeo.no/crl/a01drvw064.adeo.no_NAV%20Issuing%20CA.crt"
     dcurl "$1" "http://crl.adeo.no/crl/A01DRVW006.adeo.no_NAV%20Issuing%20CA%20Intern(1).crt"
     dcurl "$1" "http://crl.adeo.no/crl/A01DRVW269.adeo.no_NAV%20Issuing%20CA%20ekstern(1).crt"
+}
 
+function download_certs_dev {
     dcurl "$1" "http://crl.preprod.local/crl/B27pkiroot2012_B27%20Root%20CA(1).crt"
     dcurl "$1" "http://crl.preprod.local/crl/B27pkisub2012_B27%20Sub%20CA(4).crt"
     dcurl "$1" "http://crl.preprod.local/crl/B27DRVW009.preprod.local_B27%20Issuing%20CA(1).crt"
@@ -27,16 +29,44 @@ function download_certs {
     dcurl "$1" "http://crl.test.local/crl/D26DRVW051.test.local_D26%20Issuing%20CA%20Intern(2).crt"
 }
 
-if [ $# -ne 1 ]
-then
-    echo "Usage: $0 DEST_DIR"
+function usage {
+    echo "Usage: $0 DEST_DIR [prod|dev|all]"
+    echo
+    echo "Downloads the NAV CA bundle as individual PEM files into a directory."
+    echo
+    echo "The second parameter specifies whether the bundle should be limited to"
+    echo "certificate authorities that only issue certificates for production services."
+    echo "Defaults to 'all'."
     exit 1
-fi
+}
+
+[ $# -lt 1 ] && usage
 
 temp_dir=$(mktemp -d)
 dest_dir=$(realpath "$1")
+mkdir -p $dest_dir
 
-download_certs "$temp_dir"
+case $2 in
+"prod")
+    use_prod=1
+    use_dev=0
+    ;;
+"dev")
+    use_prod=0
+    use_dev=1
+    ;;
+"all"|"")
+    use_prod=1
+    use_dev=1
+    ;;
+*)
+    echo "Syntax error: '$2' is not a valid certificate list specification"
+    echo
+    usage
+esac
+
+[ $use_prod -eq 1 ] && download_certs_prod "$temp_dir"
+[ $use_dev -eq 1 ] && download_certs_dev "$temp_dir"
 
 OIFS="$IFS"
 IFS=$'\n'
