47721909106cde0671220c359dcd7f10ce30f465
Kim Tore Jensen <kimtjen@gmail.com>
2022-11-17 12:12:28 +0100

    Restructure; with JKS and PEM writers

---
+++ pkg/certbundle/certbundle.go
@@ -0,0 +1,124 @@
+package certbundle
+
+import (
+	"bytes"
+	"crypto/x509"
+	"encoding/hex"
+	"encoding/pem"
+	"fmt"
+	"io"
+	"strings"
+	"time"
+
+	"github.com/pavlo-v-chernykh/keystore-go"
+)
+
+type Bundle struct {
+	certs    []*x509.Certificate
+	password string
+}
+
+func New(password string) *Bundle {
+	return &Bundle{
+		certs:    make([]*x509.Certificate, 0),
+		password: password,
+	}
+}
+
+// Read PEM blocks from a reader until there are none left. Consumes all the data from the reader.
+func (bundle *Bundle) ReadAllPEM(r io.Reader) error {
+	buf := &bytes.Buffer{}
+	_, err := io.Copy(buf, r)
+	if err != nil {
+		return err
+	}
+
+	data := buf.Bytes()
+	certs := make([]*x509.Certificate, 0)
+	for {
+		var block *pem.Block
+		block, data = pem.Decode(data)
+		if block == nil {
+			break
+		}
+
+		cert, err := toCert(block)
+		if err != nil {
+			return err
+		}
+
+		certs = append(certs, cert)
+	}
+
+	bundle.certs = append(bundle.certs, certs...)
+
+	return nil
+}
+
+func (bundle *Bundle) KeyStore() keystore.KeyStore {
+	gentime := time.Now()
+	ks := keystore.KeyStore{}
+	for i, cert := range bundle.certs {
+		name := fmt.Sprintf("%04d_%s", i, certificateAlias(cert))
+		entry := &keystore.TrustedCertificateEntry{
+			Entry: keystore.Entry{
+				CreationDate: gentime,
+			},
+			Certificate: keystore.Certificate{
+				Type:    "X509",
+				Content: cert.Raw,
+			},
+		}
+		ks[name] = entry
+	}
+	return ks
+}
+
+func (bundle *Bundle) WriteJKS(w io.Writer) error {
+	return keystore.Encode(w, bundle.KeyStore(), []byte(bundle.password))
+}
+
+func (bundle *Bundle) WritePEM(w io.Writer) error {
+	for _, cert := range bundle.certs {
+		err := pem.Encode(w, &pem.Block{
+			Type:  "CERTIFICATE",
+			Bytes: cert.Raw,
+		})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (bundle *Bundle) Certificates() []*x509.Certificate {
+	result := make([]*x509.Certificate, len(bundle.certs))
+	for i, ptr := range bundle.certs {
+		cert := *ptr
+		result[i] = &cert
+	}
+	return result
+}
+
+// Convert a PEM block to a certificate.
+func toCert(block *pem.Block) (*x509.Certificate, error) {
+	return x509.ParseCertificate(block.Bytes)
+}
+
+// Generate a keytool compatible alias for a certificate.
+// Converts to lowercase and strips away non-alphanumeric characters.
+// In case no CN is defined, this function generates a name based on the signature data.
+func certificateAlias(cert *x509.Certificate) string {
+	replace := func(r rune) rune {
+		if (r >= 'a' && r <= 'z') || (r >= '0' && r <= '9') {
+			return r
+		}
+		return '_'
+	}
+	name := strings.ToLower(cert.Subject.CommonName)
+	if len(name) == 0 {
+		name = "anon_" + hex.EncodeToString(cert.Signature)[:32]
+	}
+	name = strings.Map(replace, name)
+	return name
+}
---
+++ pkg/certbundle/certbundle_test.go
@@ -0,0 +1,64 @@
+package certbundle_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/nais/certificator/pkg/certbundle"
+	"github.com/stretchr/testify/assert"
+)
+
+const password = "foobar"
+
+func bundleFromTestData() *certbundle.Bundle {
+	f, err := os.Open("../../testdata/cacert.pem")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	bundle := certbundle.New(password)
+
+	err = bundle.ReadAllPEM(f)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return bundle
+}
+
+func TestReadAllPEM(t *testing.T) {
+	bundle := bundleFromTestData()
+
+	for _, cert := range bundle.Certificates() {
+		t.Logf("CA=%v %v", cert.IsCA, cert.Subject)
+	}
+}
+
+func TestWrite(t *testing.T) {
+	const password = "foobar"
+
+	bundle := bundleFromTestData()
+
+	jksout, err := os.CreateTemp(os.TempDir(), "jks-")
+	if err != nil {
+		panic(err)
+	}
+	defer jksout.Close()
+
+	pemout, err := os.CreateTemp(os.TempDir(), "pem-")
+	if err != nil {
+		panic(err)
+	}
+	defer pemout.Close()
+
+	err = bundle.WriteJKS(jksout)
+	assert.NoError(t, err)
+
+	err = bundle.WritePEM(pemout)
+	assert.NoError(t, err)
+
+	t.Logf("JKS certificates with password %q encoded into %s", password, jksout.Name())
+	t.Logf("PEM certificates encoded into %s", jksout.Name())
+}
